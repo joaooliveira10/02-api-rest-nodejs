f16c82bbff3afaa367f3e7862c2d4df6fe3dcce8
joaooliveira10 <joaoangello10@gmail.com>
2025-08-06 22:49:39 -0400

    feat: melhora a mensagem de log ao iniciar o servidor e mantém o tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,12 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: '0.0.0.0', // Escutar em todas as interfaces
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    console.log(`HTTP Server Running on port ${env.PORT}!`)
+  })
+  .catch((err) => {
+    console.error('Error starting server:', err)
+    process.exit(1)
   })
