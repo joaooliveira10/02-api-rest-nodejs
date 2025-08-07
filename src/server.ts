33d0a805dbf774ed30294691b6b725ff17136af3
joaooliveira10 <joaoangello10@gmail.com>
2025-08-06 22:58:27 -0400

    feat: ajusta a estrutura de inicialização do servidor e melhora a mensagem de log

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import { app } from './app'
 import { env } from './env'
 
-app
-  .listen({
+app.listen(
+  {
+    host: '0.0.0.0',
     port: env.PORT,
-    host: '0.0.0.0', // Escutar em todas as interfaces
-  })
-  .then(() => {
-    console.log(`HTTP Server Running on port ${env.PORT}!`)
-  })
-  .catch((err) => {
-    console.error('Error starting server:', err)
-    process.exit(1)
-  })
+  },
+  () => {
+    console.log(`HTTP Server Running on http://0.0.0.0:${env.PORT}`)
+  },
+)
